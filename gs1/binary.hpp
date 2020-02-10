f208f5e2cb828c8aaa90adf71b42e64a17d26a68
Christophe Pijcke <cpijcke.wall@laposte.net>
2020-02-10 16:56:44 +0100

    Update binary.hpp

--- gs1/binary.hpp
+++ gs1/binary.hpp
@@ -29,16 +29,20 @@ std::ostream& binary_write(const dm_type_tres<AiItem>& i, std::ostream& o)
 	return o;
 }
 
+/*
 struct binary_printable
 {
 	const code& c_;
+	const char start_;
 	const char sep_;
 
 	std::ostream& write(std::ostream& o) const
 	{
 		bool needSep = false;
 
+		o << start_;
 		if (c_.gtin.defined())          { if (needSep) { o << sep_; } binary_write(c_.gtin, o); needSep = false; } // If separator needed, tell it.
+		if (c_.fablot.defined())	{ if (needSep) { o << sep_; } binary_write(c_.fablot, o); needSep = true; } // If separator needed, tell it.
 		if (c_.itemComponent.defined()) { if (needSep) { o << sep_; } binary_write(c_.itemComponent, o); needSep = false; } // If separator needed, tell it.
 
 		return o;
@@ -46,17 +50,86 @@ struct binary_printable
 };
 
 template<typename CodeT>
-binary_printable binary(CodeT c, const char sep)
+binary_printable binary(CodeT c, const char start, const char sep)
 {
-	binary_printable hp { c, sep };
+	binary_printable hp { c, start, sep };
 	return hp;
+} */
+
+
+std::ostream& binary_write_code(std::ostream& o, const code& c, char start, char sep)
+{
+	bool needSep = false;
+
+	o << start;
+	if (c.gtin.defined())		{ if (needSep) { o << sep; } binary_write(c.gtin, o); needSep = false; } // If separator needed, tell it.
+	if (c.fablot.defined())		{ if (needSep) { o << sep; } binary_write(c.fablot, o); needSep = true; } // If separator needed, tell it.
+	if (c.itemComponent.defined())	{ if (needSep) { o << sep; } binary_write(c.itemComponent, o); needSep = false; } // If separator needed, tell it.
+
+	return o;
+}
+
+class binary_serializable
+{
+        code& c_;
+	char startChar_;
+	char sepChar_;
+
+public:
+        binary_serializable(code& c, char startChar, char sepChar) : c_(c), startChar_(startChar), sepChar_(sepChar) { }
+
+        std::ostream& write(std::ostream& o) const
+        {
+                return binary_write_code(o, c_, startChar_, sepChar_);
+        }
+
+        std::istream& read(std::istream& i)
+        {
+                // TODO return binary_read_code(i, c_);
+        }
+};
+
+class binary_serializable_const
+{
+        const code& c_;
+	char startChar_;
+	char sepChar_;
+
+public:
+        binary_serializable_const(const code& c, char startChar, char sepChar) : c_(c), startChar_(startChar), sepChar_(sepChar) { }
+
+        std::ostream& write(std::ostream& o) const
+        {
+                return binary_write_code(o, c_, startChar_, sepChar_);
+        }
+};
+
+template<typename CodeT>
+typename std::enable_if<std::is_const<CodeT>::value, binary_serializable_const>::type
+binary(CodeT c, char start, char sep)
+{
+        binary_serializable_const hp(c, start, sep);
+        return hp;
+}
+
+template<typename CodeT>
+typename std::enable_if<!std::is_const<CodeT>::value, binary_serializable>::type
+binary(CodeT c, char start, char sep)
+{
+        binary_serializable hp(c, start, sep);
+        return hp;
 }
 
 } // namespace gs1
 
-std::ostream& operator<<(std::ostream& o, const gs1::binary_printable& h)
+std::ostream& operator<<(std::ostream& o, const gs1::binary_serializable& h)
 {
-	return h.write(o);
+        return h.write(o);
+}
+
+std::istream& operator>>(std::istream& i, gs1::binary_serializable&& h)
+{
+        return h.read(i);
 }
 
 #endif // #define GS1_BINARY
