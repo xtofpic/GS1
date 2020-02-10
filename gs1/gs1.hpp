205c6ecb92d0876b0715e8c11d989764d67c84a9
Christophe Pijcke <cpijcke.wall@laposte.net>
2020-02-10 16:54:17 +0100

    Update gs1.hpp

--- gs1/gs1.hpp
+++ gs1/gs1.hpp
@@ -10,11 +10,28 @@
 namespace gs1
 {
 
+struct AiFabLot
+{
+	static constexpr const char* ai = "10";
+	static constexpr const char* validation = "an..20";
+	using data_type = std::string;
+	static data_type parse(const std::string& src) { return src; }
+};
+
 struct AiGtin
 {
 	static constexpr const char* ai = "00";
 	static constexpr const char* validation = "n18";
 	using data_type = std::string;
+	static data_type parse(const std::string& src) { return src; }
+};
+
+struct AiTest
+{
+	static constexpr const char* ai = "05";
+	static constexpr const char* validation = "n5";
+	using data_type = int64_t;
+	static data_type parse(const std::string& src) { return std::atoi(src.c_str()); }
 };
 
 struct AiItemComponent
@@ -26,29 +43,40 @@ struct AiItemComponentA
 {
 	static constexpr const char* validation = "n14";
 	using data_type = int64_t;
+	static data_type parse(const std::string& src) { return std::atoi(src.substr(0, 14).c_str()); }
 };
 
 struct AiItemComponentB
 {
 	static constexpr const char* validation = "n2";
 	using data_type = int64_t;
+	static data_type parse(const std::string& src) { return std::atoi(src.substr(14, 2).c_str()); }
 };
 
 struct AiItemComponentC
 {
 	static constexpr const char* validation = "n2";
 	using data_type = int64_t;
+	static data_type parse(const std::string& src) { return std::atoi(src.substr(16, 2).c_str()); }
 };
 
 template<class T>
-uint16_t extract_length()
+int16_t extract_length()
 {
 	uint8_t pos = (T::validation[0] == 'n') ? 1 : 2;
-	uint16_t toReturn = 0;
+	int16_t toReturn = 0;
 
 	if (T::validation[pos] != '.')
 	{
 		toReturn = std::atoi(&T::validation[pos]);
+		if (toReturn == 0)
+		{
+			throw std::runtime_error(std::string("extract lenght from ") + T::validation + " failed.");
+		}
+	}
+	else
+	{
+		toReturn = -std::atoi(&T::validation[(pos + 2)]);
 		if (toReturn == 0)
 		{
 			throw std::runtime_error(std::string("extract lenght from ") + T::validation + " failed.");
@@ -61,19 +89,40 @@ template<class T>
 typename std::enable_if<std::is_same<typename T::data_type, std::string>::value, bool>::type
 dm_check(const std::string& i, bool throwIfBad)
 {
-	if (extract_length<T>() == 0)
+	int16_t length = extract_length<T>();
+
+	if (length == 0)
 	{
 		if (throwIfBad)
 		{
 			throw std::runtime_error(std::string("Data does not fullfill pattern: ") + T::validation);
 		}
-		return true;
+		return false;
 	}
 
-	if (throwIfBad && i.length() != extract_length<T>())
+	if (length < 0)		// Dynamic length
 	{
-		throw std::runtime_error(std::string("Data does not fullfill pattern length: ") + T::validation);
+		if (i.length() > (-length))
+		{
+			if (throwIfBad)
+			{
+				throw std::runtime_error(std::string("Data length does not fullfill pattern: ") + T::validation);
+			}
+			return false;
+		}
 	}
+	else			// Fixed length
+	{
+		if (i.length() != length)
+		{
+			if (throwIfBad)
+			{
+				throw std::runtime_error(std::string("Data length does not fullfill pattern: ") + T::validation);
+			}
+			return false;
+		}
+	}
+
 
 	// If pattern is 'n', assert than all char is 0-9 + '-' with eventually one '.'
 	if (T::validation[0] == 'n')
@@ -108,8 +157,7 @@ dm_check(const std::string& i, bool throwIfBad)
 			}
 		}
 	}
-
-	return (i.length() == extract_length<T>());
+	return true;
 }
 
 template<class T>
@@ -118,7 +166,8 @@ dm_check(const int64_t& i, bool throwIfBad)
 {
 	if (extract_length<T>() != 0)
 	{
-		if (i >= std::pow(10, extract_length<T>()))
+		int16_t maxchar = ((i < 0) ? -1 : 0) + extract_length<T>();
+		if (std::abs(i) >= std::pow(10, maxchar))
 		{
 			if (throwIfBad)
 			{
@@ -162,6 +211,10 @@ public:
 
 	bool defined() const { return defined_; }
 
+	void clear() { defined_ = false; }
+
+	void parse(const std::string& src) { defined_ = false; data_ = Ai::parse(src); dm_check<Ai>(data_, true); defined_ = true; }
+
 	static bool isValid(const data_type& d) { return dm_check<Ai>(d, false); }
 
 	data_type data_;
@@ -179,15 +232,36 @@ public:
 	dm_type<AiItemComponentB> b_;
 	dm_type<AiItemComponentC> c_;
 
+	void parse(const std::string& src)
+	{
+		a_.defined_ = false;
+		b_.defined_ = false;
+		c_.defined_ = false;
+		a_.data_ = AiItemComponentA::parse(src);
+		b_.data_ = AiItemComponentB::parse(src);
+		c_.data_ = AiItemComponentC::parse(src);
+	       	dm_check<AiItemComponentA>(a_.data_, true);
+	       	dm_check<AiItemComponentA>(b_.data_, true);
+	       	dm_check<AiItemComponentA>(c_.data_, true);
+		a_.defined_ = true;
+		b_.defined_ = true;
+		c_.defined_ = true;
+	}
+
+	void clear() { a_.clear(); b_.clear(); c_.clear(); }
 	bool defined() const { return (a_.defined() && b_.defined() && c_.defined()); }
 };
 
 using gtin = dm_type<AiGtin>;
+using fablot = dm_type<AiFabLot>;
 using itemComponent = dm_type_tres<AiItemComponent>;
+using test = dm_type<AiTest>;
 
 struct code
 {
 	dm_type<AiGtin> gtin;
+	dm_type<AiFabLot> fablot;
+	dm_type<AiTest> test;
 	dm_type_tres<AiItemComponent> itemComponent;
 };
 
